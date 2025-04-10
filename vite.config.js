48bdd394211871a6eb4949a522bd2a5ee079b03d
Sarthak Suwan <sarthaksu3010@gmail.com>
2025-04-11 00:01:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/HybridQuickSortViz/", // make sure this starts and ends with /
+  base: "/HybridQuickSortViz", 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
