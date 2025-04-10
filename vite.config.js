b0eb9c86c1c9132c1ef001eafaba9fa0e7ec7a1e
Sarthak Suwan <sarthaksu3010@gmail.com>
2025-04-10 23:44:43 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/HybridQuickSortViz/", // replace with your repo name
-})
+  base: "/HybridQuickSortViz/", // make sure this starts and ends with /
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
