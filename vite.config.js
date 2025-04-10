ebf2f87f04cb80c0f3703080ce848c547e9a1984
Sarthak Suwan <sarthaksu3010@gmail.com>
2025-04-10 23:32:30 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "HybridQuickSortViz", // replace with your repo name
+  base: "/HybridQuickSortViz/", // replace with your repo name
 })
